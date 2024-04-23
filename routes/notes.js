ba19a704852ace13cf3945a62d5db101dd316c8e
Dev Karan <dev882624@gmail.com>
2024-04-23 14:16:44 +0530

    deleted endpoint created

--- routes/notes.js
+++ routes/notes.js
@@ -50,7 +50,14 @@ router.put('/updatenotes/:id', fetchuser, async (req, res) => {
         if (tag) { newnote.tag = tag }
 
         // find the note
-        let note = await Notes.findById(req.params.id)
+        let note;
+        try {
+            note = await Notes.findById(req.params.id)
+            
+        } catch (error) {
+            console.error(error.message)
+            return res.status(400).send("Bad request id error")
+        }
         if (!note) { return res.status(404).send("not found") }
 
         if (note.user.toString() != req.user.id) {
@@ -59,6 +66,32 @@ router.put('/updatenotes/:id', fetchuser, async (req, res) => {
 
         note = await Notes.findByIdAndUpdate(req.params.id, {$set: newnote}, {new:true})
         res.json({newnote})
+
+    } catch (error) {
+        console.log("in the error block")
+        console.error(error.message);
+    }
+})
+
+// Deleting a note
+
+router.delete('/deletenotes/:id', fetchuser, async (req, res) => {
+    try {
+        // find the note
+        let note;
+        try {
+            note = await Notes.findById(req.params.id)
+        } catch (error) {
+            return res.status(400).send("bad request deleted note")
+        }
+        if (!note) { return res.status(404).send("not found") }
+
+        if (note.user.toString() != req.user.id) {
+            return res.status(400).send("bad request")
+        }
+
+        note = await Notes.findByIdAndDelete(req.params.id)
+        res.json({"sucess": "note deleted sucessfully", note})
 
     } catch (error) {
         console.log("in the error block")
